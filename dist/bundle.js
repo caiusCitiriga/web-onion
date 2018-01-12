46ca2ce78a955a604a25398922063cfa1160702f
Caius Citiriga <nbkmaxine@gmail.com>
2018-01-12 22:08:47 +0100

    builded bundle

--- dist/bundle.js
+++ dist/bundle.js
@@ -1342,7 +1342,7 @@ exports.WOHelpManager = WOHelpManager;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.GENERAL_CONF = {
-    version: '3.0.1'
+    version: '3.0.2'
 };
 
 
