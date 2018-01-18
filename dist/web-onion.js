6be60ce9e6d2e51f9c1c9520bffb8c57faed6762
Caius Citiriga <nbkmaxine@gmail.com>
2018-01-19 00:17:19 +0100

    fresh build

--- dist/web-onion.js
+++ dist/web-onion.js
@@ -152,7 +152,7 @@ exports.WORenderer = WORenderer;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.GENERAL_CONF = {
-    version: '3.0.5'
+    version: '3.0.7-beta.1'
 };
 
 
