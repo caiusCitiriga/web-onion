bbc7aa3e4c6c764b5fb37d8bf797a25e7fdb04c9
Caius Citiriga <nbkmaxine@gmail.com>
2018-02-19 20:05:49 +0100

    documented class

--- src/core/wo-parser.core.ts
+++ src/core/wo-parser.core.ts
@@ -1,4 +1,3 @@
-import { WOCommandSet } from './../entities/wo-command-set.entity';
 import { WebOnionSDK } from '../web-onion';
 
 import { WOInput } from './wo-input.core';
@@ -8,12 +7,13 @@ import { WODispatcherConfiguration } from '../entities/wo-dispatcher-configurati
 import { WOSeverityEnum } from '../enums/wo-severity.enum';
 import { WOFlag } from '../entities/wo-flag.entity';
 import { WORenderer } from './wo-renderer.core';
+import { WOCommandSet } from '../entities/wo-command-set.entity';
 
 export class WOParser {
 
     private sdk: WebOnionSDK;
     private history_index = 0;
-    private session_history: WOCommandSet[] = [];
+    private session_commands_history: WOCommandSet[] = [];
 
     private command_set: WOCommandSet = {
         command: null,
@@ -34,21 +34,47 @@ export class WOParser {
             if (this.inputIsInWaitMode()) { return; }
 
             this.assignCommandSet();
-
             //  TODO handle a missing required flag (like echo command)
             //  TODO handle wrong flag delimiters (warn user)
 
-            this.sdk.dispatcher_lib.dispatch(dispatcher_conf, this.command_set, this.sdk);
+            this.addCommandToHistory();
+            this.dispatchCommand(dispatcher_conf);
             this.resetCommandSet();
-
-            sdk.clearAfterSubmit ? sdk.input_lib.clearInput() : null;
+            this.clearCommandInputIfNeeded();
         });
     }
 
+
+    /**
+     * Assigns the command set internally
+     * 
+     * @private
+     * @memberof WOParser
+     */
     private assignCommandSet(): void {
-        this.command_set = this.parseRAWCommand(WORenderer.getVal('input.wc-input-field'));
+        this.command_set = this.parseRAWCommand(this.extractRAWCommand());
     }
 
+
+    /**
+     * Extracts the command from the input field
+     * 
+     * @private
+     * @returns {string} 
+     * @memberof WOParser
+     */
+    private extractRAWCommand(): string {
+        return WORenderer.getVal('input.wc-input-field') as string;
+    }
+
+    /**
+     * Assigns the command set internally
+     * 
+     * @private
+     * @param {string} raw_command 
+     * @returns {WOCommandSet} 
+     * @memberof WOParser
+     */
     private parseRAWCommand(raw_command: string): WOCommandSet {
         const command_set: WOCommandSet = {
             command: null,
@@ -65,6 +91,27 @@ export class WOParser {
     }
 
     /**
+     * Pushes the current command_set in the session's commands histroy
+     * 
+     * @private
+     * @memberof WOParser
+     */
+    private addCommandToHistory(): void {
+        this.session_commands_history.push(this.command_set);
+    }
+
+    /**
+     * Dispatches the command using the SDK's dispatcher
+     * 
+     * @private
+     * @param {WODispatcherConfiguration[]} dispatcher_conf 
+     * @memberof WOParser
+     */
+    private dispatchCommand(dispatcher_conf: WODispatcherConfiguration[]): void {
+        this.sdk.dispatcher_lib.dispatch(dispatcher_conf, this.command_set, this.sdk);
+    }
+
+    /**
      * Resets the command set
      * 
      * @private
@@ -75,6 +122,24 @@ export class WOParser {
         this.command_set.flags = null;
     }
 
+    /**
+     * If the configs is set to clear the input, it will clear it
+     * 
+     * @private
+     * @memberof WOParser
+     */
+    private clearCommandInputIfNeeded(): void {
+        this.sdk.clearAfterSubmit ? this.sdk.input_lib.clearInput() : null;
+    }
+
+    /**
+     * If the input is in "wait mode", meaning that the command should be treated as a info from the user.
+     * If so, it will have a wc-input-wait class on it and returns TRUE
+     * 
+     * @private
+     * @returns {boolean} 
+     * @memberof WOParser
+     */
     private inputIsInWaitMode(): boolean {
         return $('input.wc-input-field').hasClass('wc-input-wait');
     }
